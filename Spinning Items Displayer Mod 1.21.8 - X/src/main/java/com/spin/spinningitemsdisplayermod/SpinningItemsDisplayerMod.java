bb9918c2566ad6e15afa291516779a9d7c34098b
Samy10063 <168396984+Samy10063@users.noreply.github.com>
2025-12-14 22:19:55 -0400

    Add files via upload

---
+++ Spinning Items Displayer Mod 1.21.8 - X/src/main/java/com/spin/spinningitemsdisplayermod/SpinningItemsDisplayerMod.java
@@ -0,0 +1,14 @@
+package com.spin.spinningitemsdisplayermod;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.ModContainer;
+
+@Mod(SpinningItemsDisplayerMod.MODID)
+public class SpinningItemsDisplayerMod {
+    public static final String MODID = "spinningitemsdisplayermod";
+
+    public SpinningItemsDisplayerMod(IEventBus modEventBus, ModContainer modContainer) {
+    }
+
+}
---
+++ Spinning Items Displayer Mod 1.21.8 - X/src/main/java/com/spin/spinningitemsdisplayermod/SpinningItemsDisplayerModClient.java
@@ -0,0 +1,11 @@
+package com.spin.spinningitemsdisplayermod;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+
+@Mod(value = SpinningItemsDisplayerMod.MODID, dist = Dist.CLIENT)
+@EventBusSubscriber(modid = SpinningItemsDisplayerMod.MODID, value = Dist.CLIENT)
+public class SpinningItemsDisplayerModClient {
+
+}
